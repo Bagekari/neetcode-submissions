ed565f91dc1f7f90d8f83031f158e5ad71251163
Bagekari <47973537+Bagekari@users.noreply.github.com>
2026-06-06 15:29:23 +0530

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        int n = words.length;
+        int[] map = new int[26];
+
+        for (int i = 0; i < order.length(); ++i) {
+            char c = order.charAt(i);
+            map[c - 'a'] = i;
+        }
+
+        for (int i = 1; i < n; ++i)
+            if (!compare(words[i - 1], words[i], map))
+                return false;
+        
+        return true;
+    }
+
+    private boolean compare(String a, String b, int[] map) {
+        for (int i = 0; i < a.length() && i < b.length(); ++i) {
+            if (Integer.compare(map[a.charAt(i) - 'a'], map[b.charAt(i) - 'a']) > 0) {
+                return false;
+            } else if (Integer.compare(map[a.charAt(i) - 'a'], map[b.charAt(i) - 'a']) < 0) {
+                return true;
+            }
+        }
+        
+        return a.length() <= b.length();
+    }
+}
